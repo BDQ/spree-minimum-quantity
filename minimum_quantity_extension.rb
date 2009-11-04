f66ab6140031413987a55fb881bd2a8d1ec86d7e
Brian Quinn <briandquinn@gmail.com>
2009-11-04 18:20:54 +0000

    Allows removing of items from cart qty =0

--- minimum_quantity_extension.rb
+++ minimum_quantity_extension.rb
@@ -22,7 +22,7 @@ class MinimumQuantityExtension < Spree::Extension
 			def must_meet_minimum
 				minimum = variant.is_master? ? variant.product.minimum_quantity : variant.minimum_quantity
 				minimum ||= 0
-				errors.add(:quantity, "does not meet minimum quantity required") if quantity < minimum
+				errors.add(:quantity, "does not meet minimum quantity required") if quantity < minimum && quantity != 0
 			end
 		end
 		
