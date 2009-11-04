ce9cdc8e1f4cf9d86c22d1097e5e862801359ebf
Brian Quinn <briandquinn@gmail.com>
2009-11-04 17:58:36 +0000

    Fixed validator for non minimum quantity products

--- minimum_quantity_extension.rb
+++ minimum_quantity_extension.rb
@@ -16,12 +16,12 @@ class MinimumQuantityExtension < Spree::Extension
 		Variant.additional_fields += [ :name => 'Minimum Quantity', :options => { :size => 4}  ]
 		
 		LineItem.class_eval do 
-	 
+	 		validate :must_meet_minimum
 			
-			alias :core_validate :validate
-			def validate
-				core_validate
+			private
+			def must_meet_minimum
 				minimum = variant.is_master? ? variant.product.minimum_quantity : variant.minimum_quantity
+				minimum ||= 0
 				errors.add(:quantity, "does not meet minimum quantity required") if quantity < minimum
 			end
 		end
