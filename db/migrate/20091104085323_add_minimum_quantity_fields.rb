1c79c017162f5ffb399e62d3dc28db1248e09222
Brian Quinn <briandquinn@gmail.com>
2009-11-04 12:44:57 +0000

    Fixed type in migration

--- db/migrate/20091104085323_add_minimum_quantity_fields.rb
+++ db/migrate/20091104085323_add_minimum_quantity_fields.rb
@@ -1,11 +1,11 @@
 class AddMinimumQuantityFields < ActiveRecord::Migration
   def self.up
 		add_column :variants, :minimum_quantity, :integer
-		add_column :products, :minimun_quantity, :integer
+		add_column :products, :minimum_quantity, :integer
   end
 
   def self.down
-		remove_column :products, :minimun_quantity
+		remove_column :products, :minimum_quantity
 		remove_column :variants, :minimum_quantity
   end
 end
